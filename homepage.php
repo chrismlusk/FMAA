c34d6a621d9da0a5cc0334cbabd318dd8ac25193
chrismlusk <chris.m.lusk@gmail.com>
2016-03-01 16:36:51 -0500

    update homepage

---
+++ homepage.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: Homepage Template */ get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<h1>FMAA homepage</h1>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
